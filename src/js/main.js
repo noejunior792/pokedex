460ee199ad3fc380da16d21f4bd03fb126ea909e
Noé Júnior <noedombaxe@gmail.com>
2024-06-24 21:23:12 +0100

    feat: add pokemon.name in this file

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,10 @@ const offset = 0
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
 
-function convertLanguagueToHtml(language) {
-  return `<li class="languague">
+function convertPokemonToHtml(pokemon) {
+  return `<li class="pokemon">
                 <span class="number">#001</span>
-                <span class="name">${language.name}</span>
+                <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="tpyes">
@@ -15,20 +15,20 @@ function convertLanguagueToHtml(language) {
                     </ol>
 
 
-                    <img src="https://skillicons.dev/icons?i=javascript" alt=${language.name}/>
+                    <img src="https://skillicons.dev/icons?i=javascript" alt=${pokemon.name}/>
                 </div>
             </li>`
 }
 
-console.log(document.getElementsByClassName('language'))
+console.log(document.getElementsByClassName('pokemon'))
 
 fetch(url)
   .then((response) => response.json())
   .then((jsonBody) => jsonBody.results)
-  .then((languageList) => {
-    for (let i = 0; i < languageList.length; i++) {
-      const language = languageList[i];
-      console.log(convertLanguagueToHtml(language))
+  .then((pokemonList) => {
+    for (let i = 0; i < pokemonList.length; i++) {
+      const pokemon = pokemonList[i];
+      console.log(convertPokemonToHtml(pokemon))
 
     }
   })
