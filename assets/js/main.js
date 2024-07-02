99e336aaab407e927ec809254027b1edda0aab0c
Noé Júnior <noedombaxe@gmail.com>
2024-07-02 14:04:35 +0100

    feat: create a list function for pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const offset = 0
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
 
-function convertPokemonToHtml(pokemon) {
+function convertPokemonToLi(pokemon) {
   return `<li class="pokemon">
                 <span class="number">#001</span>
                 <span class="name">${pokemon.name}</span>
@@ -20,16 +20,15 @@ function convertPokemonToHtml(pokemon) {
             </li>`
 }
 
-console.log(document.getElementsByClassName('pokemon'))
+const pokemonList = document.getElementById('pokemonList');
 
 fetch(url)
   .then((response) => response.json())
   .then((jsonBody) => jsonBody.results)
-  .then((pokemonList) => {
-    for (let i = 0; i < pokemonList.length; i++) {
-      const pokemon = pokemonList[i];
-      console.log(convertPokemonToHtml(pokemon))
-
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToLi(pokemon)
     }
   })
   .catch((error) => console.log(error))
