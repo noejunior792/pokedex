35fdcc766e24aa404a2f74f31c32c745f5a4ddd9
Noé Júnior <noedombaxe@gmail.com>
2024-07-13 13:19:36 +0100

    🔥feat: Handling multiple requests in parallel.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,19 @@
+function convertPokemonTypesToLi(pokemonTypes){
+  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
+}
 
 function convertPokemonToLi(pokemon) {
   return `<li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="tpyes">
-                        <li class="types">Secure</li>
-                        <li class="types">Fast</li>
+                        ${convertPokemonTypesToLi(pokemon.types).join('')}
                     </ol>
 
 
-                    <img src="https://skillicons.dev/icons?i=javascript" alt=${pokemon.name}/>
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt=${pokemon.name}/>
                 </div>
             </li>`
 }
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,18 +1,16 @@
 const pokeApi = {};
 
+pokeApi.getPokemonDetail = (pokemon) =>{
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 0) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
           .then((response) => response.json())
           .then((jsonBody) => jsonBody.results)
+          .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+          .then((detailRequest) => Promise.all(detailRequest))
+          .then((pokemonsDetails) => pokemonsDetails)
 }
-
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4')
-]).then((results) => {
-    console.log(results);
-})
