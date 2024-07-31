05409e8b4714d412f97c9bf5a5d063e97d1ef424
Noé Júnior <noedombaxe@gmail.com>
2024-07-31 21:32:09 +0100

    🔥 refactor: optimize pokemon loading to reduce carbon footprint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,64 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-const limit = 5
+const limit = 5;
 let offset = 0;
 
+function createPokemonItem(pokemon) {
+    const li = document.createElement('li');
+    li.classList.add('pokemon', pokemon.type);
 
-function loadPokemonItens(offset, limit){
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) => `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `).join('')
-        pokemonList.innerHTML += newHtml
-    })
+    const number = document.createElement('span');
+    number.classList.add('number');
+    number.textContent = `#${pokemon.number}`;
+
+    const name = document.createElement('span');
+    name.classList.add('name');
+    name.textContent = pokemon.name;
+
+    const detail = document.createElement('div');
+    detail.classList.add('detail');
+
+    const types = document.createElement('ol');
+    types.classList.add('types');
+    pokemon.types.forEach(type => {
+        const typeLi = document.createElement('li');
+        typeLi.classList.add('type', type);
+        typeLi.textContent = type;
+        types.appendChild(typeLi);
+    });
+
+    const img = document.createElement('img');
+    img.src = pokemon.photo;
+    img.alt = pokemon.name;
+
+    detail.appendChild(types);
+    detail.appendChild(img);
+
+    li.appendChild(number);
+    li.appendChild(name);
+    li.appendChild(detail);
+
+    return li;
 }
 
-loadPokemonItens(offset, limit)
+function appendPokemons(pokemons) {
+    const fragment = document.createDocumentFragment();
+    pokemons.forEach(pokemon => {
+        const pokemonItem = createPokemonItem(pokemon);
+        fragment.appendChild(pokemonItem);
+    });
+    pokemonList.appendChild(fragment);
+}
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        appendPokemons(pokemons);
+    });
+}
+
+loadPokemonItems(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    loadPokemonItens(offset, limit)
-})
+    offset += limit;
+    loadPokemonItems(offset, limit);
+});
